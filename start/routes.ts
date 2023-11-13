9b4b4341a3911ae173eab12d0653bdb697cefa39
Supra0km <miguelcastroferreira5@gmail.com>
2023-11-13 10:24:31 -0300

    sim

--- start/routes.ts
+++ start/routes.ts
@@ -4,8 +4,10 @@ Route.get('/', async () => {
   return { hello: 'world' }
 })
 
-Route.post('/register', 'AuthController.register')
-Route.post('/login', 'AuthController.login')
-Route.group(()=> {
+Route.post("/register", "AuthController.register")
+Route.post("/login", "AuthController.login")
+Route.group(() =>{
   Route.put("/user", "UsersController.update")
+  Route.post("push", "SendPushesController.send")
 }).middleware('auth')
+  
